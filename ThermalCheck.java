1287f6b4787fa69656027fd62595490e213d0169
aranbarri <aranbarriv@gmail.com>
2025-04-05 14:05:42 +0200

    Update ThermalCheck.java

--- ThermalCheck.java
+++ ThermalCheck.java
@@ -6,38 +6,49 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Properties;
 
-public class ThermalProducer {
-
-    public static double getCpuTemperature() {
-        try {
+public class ThermalProducer 
+{
+	static int waitSeconds;
+	
+    public static double getCpuTemperature() 
+    {
+        try 
+        {
             String tempPath = "/host_thermal/thermal_zone0/temp";
             String temp = Files.readString(Path.of(tempPath)).trim();
             return Double.parseDouble(temp) / 1000.0;
-        } catch (Exception e) {
+        } catch (Exception e) 
+        {
             e.printStackTrace();
             return -1; // Valor indicando error
         }
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception 
+    {
         Properties props = new Properties();
         props.put("bootstrap.servers", "kafka:9092");
         props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
-        KafkaProducer<String, String> producer = new KafkaProducer<>(props);
+        @SuppressWarnings("resource")
+		KafkaProducer<String, String> producer = new KafkaProducer<>(props);
 
-        while (true) {
+        while (true) 
+        {
             double cpuTemp = getCpuTemperature();
 
-            if (cpuTemp != -1) {
+            if (cpuTemp != -1) 
+            {
                 producer.send(new ProducerRecord<>("cpu-temperature", "cpu", String.format("%.2f", cpuTemp)));
-                System.out.println("CPU Temp sent: " + cpuTemp);
-            } else {
+                waitSeconds=1;
+            } else 
+            {
                 System.err.println("Error obteniendo temperatura");
+                waitSeconds=5; //let's wait a little bit more
             }
 
-            Thread.sleep(5000); // cada 5 segundos
+            Thread.sleep(waitSeconds);
         }
     }
 }
